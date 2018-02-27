9067524a0252c4d9e172e06c54a81985c26bd6b2
mcchan1 <mcchan1@gmail.com>
2018-02-27 16:23:49 -0500

    revised IPFS - accepts docs, jpg.
    dropped Ipfs -mini to and installed regular ipfs-api. refactored code to send files to IPFS instead of text only

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import {Table, Grid, Button } from 'react-bootstrap';
+import {Table, Grid, Button, Form } from 'react-bootstrap';
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import web3 from './web3';
 import ipfs from './ipfs';
@@ -12,148 +12,90 @@ import storehash from './storehash';
 
 class App extends Component {
  
-  state = {
-    value: '',
-    result: ' ', 
-    txReceipt: '',
-    ethAddress: '', 
-    blockNumber:'',
-    transactionHash:'',
-    gasUsed:'' 
-  };
- 
-  async componentDidMount() {
-
-    console.log('componentDidMount');   
-  }
-
-  onSubmit = async (event) => {
-    event.preventDefault();
-
-    //bring in user's metamask account address
-    const accounts = await web3.eth.getAccounts();
+    state = {
+      ipfsHash:null,
+      buffer:''  
+    };
    
-    console.log('Sending from Metamask account: ' + accounts[0]);
+    async componentDidMount() {
+      console.log('componentDidMount');   
 
-    //obtain contract address from storehash.js
-    const ethAddress= await storehash.options.address;
-    this.setState({ethAddress});
-
-    //send the IPFS hash value to Ethereum w/ user's Metamask account
-    await ipfs.add(this.state.value, (err, result) =>{
-        console.log(err,result);
-
-        // Set this.setState 'result' to value of function parameter result 
-        this.setState({ result }); // es6 syntax 
-        // this.setState({ result: result }); // long-form  
-
-        // call a contract method and .send IPFS hash to etheruem contract 
-        //return the transaction hash from the ethereum contract
-        //see, this https://web3js.readthedocs.io/en/1.0/web3-eth-contract.html#methods-mymethod-send
-        storehash.methods.sendHash(this.state.result).send({
-          from: accounts[0] 
-      }, (error, transactionHash)=>{
-          console.log(transactionHash);
-          this.setState({transactionHash});
-        });
-    }) //await for ipfs.add
-  }; //onSubmit end
-
-  onClick = async () => {
-
-    try{
-     
-      this.setState({blockNumber:"waiting.."});
-      this.setState({gasUsed:"waiting..."});
-
-      //get Transaction Receipt in console on click
-      //See: https://web3js.readthedocs.io/en/1.0/web3-eth.html#gettransactionreceipt
-      await web3.eth.getTransactionReceipt(this.state.transactionHash, (err, txReceipt)=>{
-        console.log(err,txReceipt);
-        this.setState({txReceipt});
-
-      }); //await for getTransactionReceipt
-
-      await this.setState({blockNumber: this.state.txReceipt.blockNumber});
-      await this.setState({gasUsed: this.state.txReceipt.gasUsed});    
-    } //try
-    catch(error){
-      console.log(error);
     }
-  } //onClick
- 
-  render() {
-    
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Simple Ethereum and IPFS with Create React App</h1>
-        </header>
-        
-        <hr />
-      <Grid>
-        <form onSubmit={this.onSubmit}>
-          <p> Data to send to IPFS </p>
 
-            <input
-              value = {this.state.value}
-              onChange = {event => this.setState({value: event.target.value})}
-            />
-           
-           <button> Send it </button>
+  captureFile =(event) => {
+      event.stopPropagation()
+      event.preventDefault()
+      const file = event.target.files[0]
+      let reader = new window.FileReader()
+      reader.readAsArrayBuffer(file)
+      reader.onloadend = () => this.convertToBuffer(reader)    
+    };
 
-         </form>
 
+  convertToBuffer = async(reader) => {
+
+    //file is converted to a buffer to prepare for uploading to IPFS
+      const buffer = await Buffer.from(reader.result);
+    //set this buffer with es6
+      this.setState({buffer});
+  };
+
+    onSubmit = async (event) => {
+      event.preventDefault();
+      
+      //save document to IPFS,return its hash#, and set hash# to state
+      await ipfs.add(this.state.buffer, (err, ipfsHash) => {
+        console.log(err,ipfsHash);
+        this.setState({ ipfsHash:ipfsHash[0].hash });
+      }) //await ipfs add
+    }; //onSubmit end
+
+   
+    render() {
+      
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1>Simple Ethereum and IPFS with Create React App</h1>
+          </header>
+          
           <hr />
 
-          <Button onClick = {this.onClick}> Get Transaction Receipt </Button>
+        <Grid>
+          <p> Data to send to IPFS </p>
+          <Form onSubmit={this.onSubmit}>
+       
+                <input 
+                  type = "file"
+                  onChange = {this.captureFile}
+                />
 
-            <Table bordered responsive>
-            <thead>
-              <tr>
-                <th>Tx Receipt Category</th>
-                <th>Values</th>
-              </tr>
-            </thead>
-           
-            <tbody>
-               <tr>
-                <td>Data Sent to IPFS</td>
-                <td>{this.state.value}</td>
-           
-              </tr>
+             <Button type="submit"> Send it </Button>
+           </Form>
 
-               <tr>
-                <td>IPFS Hash # stored on Eth Contract</td>
-                <td>{this.state.result}</td>
-           
-              </tr>
-              <tr>
-                <td>Ethereum Contract Address</td>
-                <td>{this.state.ethAddress}</td>
-              </tr>
+            <hr />
 
-              <tr>
-                <td>Tx Hash # </td>
-                <td>{this.state.transactionHash}</td>
-              </tr>
+              <Table bordered responsive>
+              <thead>
+                <tr>
+                  <th>Tx Receipt Category</th>
+                  <th>Values</th>
+                </tr>
+              </thead>
+             
+              <tbody>
 
-              <tr>
-                <td>Block Number # </td>
-                <td>{this.state.blockNumber}</td>
-              </tr>
+                 <tr>
+                  <td>IPFS Hash # stored on Eth Contract</td>
+                  <td>{this.state.ipfsHash}</td>
+                </tr>
 
-              <tr>
-                <td>Gas Used</td>
-                <td>{this.state.gasUsed}</td>
-              </tr>
-          
-            </tbody>
-          </Table>
-      </Grid>
-   </div>
-    );
-  }
+              </tbody>
+            </Table>
+        </Grid>
+     </div>
+      );
+    } //render
 }
 
 export default App;
--- src/ipfs.js
+++ src/ipfs.js
@@ -1,5 +1,9 @@
 //using the infura.io node, otherwise ipfs requires you to run a daemon on your own computer/server.
-const IPFS = require('ipfs-mini');
+const IPFS = require('ipfs-api');
 const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
+
+//run with local daemon
+// const ipfsApi = require('ipfs-api');
+// const ipfs = new ipfsApi('localhost', '5001', {protocol: 'http'});
 
 export default ipfs; 
