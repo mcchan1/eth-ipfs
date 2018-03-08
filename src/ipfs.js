3cec2c166199860ad17dc13bef7378c08e411603
mcchan1 <mcchan1@gmail.com>
2018-03-08 13:01:00 -0500

    Ethereum+IPFS+Create-React-App
    A simple DApp to store an IPFS hash on the Ethereum blockchain with Create-React-App as the front end

--- src/ipfs.js
+++ src/ipfs.js
@@ -1,4 +1,4 @@
-//using the infura.io node, otherwise ipfs requires you to run a daemon on your own computer/server.
+//using the infura.io node, otherwise ipfs requires you to run a daemon on your own computer/server. See IPFS.io docs
 const IPFS = require('ipfs-api');
 const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
 
